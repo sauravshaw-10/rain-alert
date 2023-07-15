4c7cd8e7b0b5e9f70215c4ef583998f4fff2a208
Saurav Shaw <saurav.shaw10@gmail.com>
2023-07-15 14:13:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+
+import requests
+from twilio.rest import Client
+
+MY_LAT = 22.474751
+MY_LONG = 88.362289
+
+api_key = os.environ.get("OWM_API_KEY")     #go to Terminal, enter command env
+account_sid = "ACe6c45331dcea08d3e7b9c2b803666e9b"
+auth_token = "9f59cd5889a6df257c90ffa9d375eeaa"
+
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    # "exclude": "current, minutely, daily"
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+
+weather_slice = weather_data["list"][:6]
+for item in weather_slice:
+    condition_code = item["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It might rain today. Remember to bring an umbrella.",
+        from_='+14027654840',
+        to='+919818203823'
+    )
+
+
+
+# https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
